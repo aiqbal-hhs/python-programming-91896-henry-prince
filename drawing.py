f622ea820fef1b5217075ad42e4662d6806e088e
henry-prince <66706650+henry-prince@users.noreply.github.com>
2021-06-02 12:14:01 +1200

    Add files via upload

---
+++ drawing.py
@@ -0,0 +1,12 @@
+import turtle
+import time
+draw = turtle.Turtle()
+
+draw.forward(50)
+draw.left(90)
+draw.forward(50)
+draw.left(90)
+draw.forward(50)
+draw.left(90)
+draw.forward(50)
+time.sleep(10)
