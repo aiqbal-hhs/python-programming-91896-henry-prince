c83198b8902c252c229250f4ee285384987c57aa
henry-prince <henry.prince@student.hhs.school.nz>
2021-07-29 21:18:46 +1200

    idk lol

--- AS91896/Pizza.py
+++ AS91896/Pizza.py
@@ -1,9 +1,13 @@
+from typing import Text
+
+
 BigNewYorker = ['Big New Yorker', 'Big Margherita', 'Big BBQ Bacon', 'Big Triple Cheese']
 Favourites = ['Roast Veggie & Caramelised Onion with Vegan Cheese', 'Margherita', 'Beef, Bacon & Caramelized Onion', 'Meat Lovers', 'Super Supreme', 'Loaded Pepperoni With Double Toppings', 'Loaded Hawaiian With Double Toppings', 'Triple Meat & Cheese', 'BBQ Beef & Onion', 'BBQ Bacon & Mushroom', 'Italian Lovers', 'Hot & Spicy Veggie', 'Veggie Lovers']
 Deluxe = ['Hot Buffalo Fried Chicken', 'Hotter Buffalo Fried Chicken', 'Hottest Buffalo Fried chicken', 'Chicken Cranberry', 'Sticky Chicken with Honey Garlic', 'Peri Peri Chicken', 'Fried Chicken & Bacon Ranch', 'Garlic Shrimp Deluxe', 'Mega Meat Lovers', 'Apricot Chicken Deluxe', 'Chicken Deluxe', 'BBQ Chicken & Bacon Deluxe']
 ClassicValue = ['Classic Cheese', 'Americano', 'Ham & Cheese', 'Classic Vege', 'Pepperoni', 'Hawaiian', 'Cheesy Garlic', 'Beef & Onion']
 Size = ['Snack', 'Large', 'Extra Large']
-Crust = ['Pan', 'San Fransisco Style', 'Thin''n''Crispy', 'Mozzarella Stuffed Crust', 'Cheesy Garlic Stuffed Crust', 'Gluten Free']
+CrustLarge = ['Pan', 'San Fransisco Style', 'Thin''n''Crispy', 'Mozzarella Stuffed Crust', 'Cheesy Garlic Stuffed Crust', 'Gluten Free']
+Crust = ['Pan', 'San Fransisco Style', 'Thin''n''Crispy', 'Mozzarella Stuffed Crust', 'Cheesy Garlic Stuffed Crust']
 Base = ['Buffalo Sauce', 'Honey Garlic Sauce', 'Authentic Tomato', 'BBQ Sauce', 'No Sauce']
 Cheese = ['Cheese', 'Extra Cheese', 'Vegan Cheese', 'No Cheese']
 Sauce = ['Hot Chilli Drizzle', 'Buffalo Drizzle', 'BBQ Drizzle', 'Mayo Drizzle', 'Basil Drizzle', 'Honey Garlic', 'Peri Peri Drizzle', 'Aioli Drizzle', 'Apricot Drizzle', 'Ranch Drizzle']
@@ -19,6 +23,7 @@ Choices = []
 n = 0
 s = ''
 prompt = ''
+Correct = ''
 
 def NumError():
     global n 
@@ -44,40 +49,38 @@ def StrError():
         except KeyError:
             break
 
+def correct():
+    while True:
+        Correct = str(input('Please check if this information correct? (y/n) '))
+        if Correct in ('y', 'n'):
+            break
+        print('Invalid input')
+    else:
+        return()
+
+
 
 while True:
     Collect = str(input('Would you like deliverly or pickup? '))
     Collect = Collect.lower().strip().replace(' ', '')
     if Collect == 'delivery':
         Price += 3
-        FirstName = str(input('What is the first name? '))
-        LastName = str(input('What is the last name? '))
-        Address = str(input('What is the address? '))
-        prompt = 'Whats the phone number? '
-        NumError()
-        PhoneNumber = int(n)
-        print('\nFirst name: ', FirstName, '\nLast name: ', LastName, '\nAddress: ', Address, '\nPhone number: ', PhoneNumber)
-        while True:
-            Correct = str(input('Is this information correct? (y/n) '))
-            if Correct in ('y', 'n'):
-                break
-            print('Invalid input')
-        if Correct == 'n':
-            continue
-        else:
-            break
+        while Correct == '' or 'n' and True:
+            FirstName = str(input('What is the first name? '))
+            LastName = str(input('What is the last name? '))
+            Address = str(input('What is the address? '))
+            prompt = 'Whats the phone number? '
+            NumError()
+            PhoneNumber = int(n)
+            print('\nFirst name: ', FirstName, '\nLast name: ', LastName, '\nAddress: ', Address, '\nPhone number: ', PhoneNumber)
+            correct()
     elif Collect == 'pickup':
-        FirstName = str(input('What is the first name? '))
-        LastName = str(input('What is the last name? '))
-        print('\nFirst name: ', FirstName, '\nLast name: ', LastName)
-        while True:
-            Correct = str(input('Is this information correct? (y/n) '))
-            if Correct in ('y', 'n'):
-                break
-            print('Invalid input')
-        if Correct == 'n':
-            continue
-        else:
+        while Correct == '' or 'n':
+            FirstName = str(input('What is the first name? '))
+            correct()
+            LastName = str(input('What is the last name? '))
+            correct()
+            print('\nFirst name: ', FirstName, '\nLast name: ', LastName)
             break
     else:
         print('that is not an option')
@@ -92,9 +95,7 @@ while True:
     if Range in Pizzas:
         while Choice != 'Done': 
             print("\nHere is the deluxe range:\n", *Range, sep='\n')
-            prompt = "\nWhat pizza would you like? "
-            StrError()
-            Choice = str(s)
+            Choice = str(input('What pizza would you like? '))
             Choice = Choice.lower().title().strip()
             Choices.append(Choice)
             if Choice in Range:
@@ -103,10 +104,11 @@ while True:
                 Choices.append(ChoiceSize)
                 print(Choices)
                 if ChoiceSize == 'Large':
-                    ChoiceCrust = str(input('What crust would you like?\nPan, San Fransisco Style, Thin'"'"'n'"'"'Crispy, Mozzarella Stuffed Crust, Cheesy Garlic Stuffed Crust, Gluten Free\n'))
-                    ChoiceCrust = ChoiceCrust.lower().title().strip()
-                    Choices.append(ChoiceSize)
-                    print(Choices)            
+                    print(Choices)
+                if ChoiceSize in Crust:
+                        ChoiceCrust = str(input('What crust would you like?\nPan, San Fransisco Style, Thin'"'"'n'"'"'Crispy, Mozzarella Stuffed Crust, Cheesy Garlic Stuffed Crust\n'))
+                        ChoiceCrust = ChoiceCrust.lower().title().strip()
+                        Choices.append(ChoiceSize)
     else:
         print('That is not a range, try again!')
 #TODO
